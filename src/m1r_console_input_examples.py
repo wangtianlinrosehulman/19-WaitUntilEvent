a22e177a709aa0c7aa2f5e5f5deb95e03bc1be2b
wangt2 <wangt2@rose-hulman.edu>
2018-04-23 10:22:05 -0400

    Done

--- src/m1r_console_input_examples.py
+++ src/m1r_console_input_examples.py
@@ -5,11 +5,11 @@ This module demonstrates how to INPUT from the CONSOLE:
   -- strings.
 
 Authors: David Mutchler, Valerie Galluzzi, Mark Hays, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Tianlin Wang.
+"""  # Done: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 ########################################################################
-# TODO: 2.  Read and run this program.  Then do the following problems,
+# Done : 2.  Read and run this program.  Then do the following problems,
 #   putting your answers RIGHT HERE IN THIS DOCUMENT.
 #
 #   1. Write a line of code that would input an INTEGER from the
@@ -35,7 +35,7 @@ Authors: David Mutchler, Valerie Galluzzi, Mark Hays, Amanda Stouder,
 #        nothing at all (just press the Enter key)
 #      -- try them!) when running the   input_an_integer   example?
 #      Put your answer here:
-#
+#       invalid literal for int()
 #   After you have PUT YOUR ANSWERS IN THIS COMMENT as described above,
 #     a. Find someone who has had HER answer checked.
 #        Ask her to check YOUR answers to the above.
